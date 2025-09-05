bb8038be60b4ee0147e9d19f97e3064c7039a3a6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-09-05 11:59:28 +0300

    requests for token

---
+++ frontend/src/app/cryptoboard/dashboard/request.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+export const getCrypto = async () => {
+  // console.log(process.env.CRYPTO_API_KEY);
+  // const res = await axios.get(`https://api.coingecko.com/api/v3//coins/list`, {
+  //   params: { x_cg_demo_api_key: process.env.CRYPTO_API_KEY },
+  //   headers: { Accept: "application/json" },
+  // });
+
+  const res = await axios.get(
+    "https://api.coingecko.com/api/v3/coins/markets",
+    {
+      params: {
+        vs_currency: "usd", // currency
+        order: "market_cap_desc", // or "volume_desc" / "id_asc"
+        per_page: 100, // max 250
+        page: 1,
+        sparkline: false,
+        price_change_percentage: "24h", // optional
+        x_cg_demo_api_key: process.env.CRYPTO_API_KEY,
+      },
+    }
+  );
+
+  return res.data;
+};
+
+export const getPreviewCrypto = async () => {
+  console.log(process.env.CRYPTO_API_KEY);
+  const res = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=24h`,
+    {
+      params: { x_cg_demo_api_key: process.env.CRYPTO_API_KEY },
+      headers: { Accept: "application/json" },
+    }
+  );
+
+  return res.data;
+};
+
+export const getHundredCrypto = async (page: number) => {
+  console.log(process.env.CRYPTO_API_KEY);
+  const res = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${page}&sparkline=false&price_change_percentage=24h`,
+    {
+      params: { x_cg_demo_api_key: process.env.CRYPTO_API_KEY },
+      headers: { Accept: "application/json" },
+    }
+  );
+
+  return res.data;
+};
