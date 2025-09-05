f80ecd9469c4f405e3b9a0c4f168ae93d3ab37c5
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-09-05 12:00:37 +0300

    remove sidemenu and add first component

--- frontend/src/app/cryptoboard/page.tsx
+++ frontend/src/app/cryptoboard/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Sidemenu from "./sidemenu";
+import TrendingMarket from "./dashboard/trendingMarket/TrendingMarket";
 
 const Page = () => {
   return (
-    <div className="w-full min-h-screen bg-purple-600 text-white flex items-center justify-center">
-      cryptoboard
-      <Sidemenu />
+    <div className="w-full min-h-screen bg-[var(--black20)] text-[var(--black100)] flex items-center justify-center flex-col">
+      <p>Cryptoboard</p>
+      <TrendingMarket />
     </div>
   );
 };
