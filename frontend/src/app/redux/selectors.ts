cf922bb0d8d22fa79eef3c777e7039b0dae6049c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 16:27:18 +0300

    user selectors

--- frontend/src/app/redux/selectors.ts
+++ frontend/src/app/redux/selectors.ts
@@ -2,8 +2,21 @@ import { RootState } from "./store";
 
 export const name = (state: RootState) => state.user.user?.name;
 export const email = (state: RootState) => state.user.user?.email;
-export const avatar = (state: RootState) => state.user.user?.avatar;
+export const avatarLink = (state: RootState) => state.user.user?.avatar.link;
+export const avatarName = (state: RootState) => state.user.user?.avatar.name;
 export const isLoading = (state: RootState) => state.user.isLoading;
 export const isLoggedIn = (state: RootState) => state.user.isLoggedIn;
+export const userError = (state: RootState) => state.user.error;
 
 export const user = (state: RootState) => state.user;
+// export const userCreateYear = (state: RootState) => state.user.user?.createdAt;
+// export const userCreateYear = (state: RootState) => {
+//   return Number(
+//     state.user.user?.createdAt?.toString().split("").slice(0, 4).join("")
+//   );
+// };
+export const userCreateYear = (state: RootState) => {
+  return state.user.user?.createdAt?.toString().split("").slice(0, 4).join("");
+};
+
+export const userTokens = (state: RootState) => state.user.user?.tokens || [];
