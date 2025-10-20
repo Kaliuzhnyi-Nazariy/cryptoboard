9d1712e21aaca451b924e0176ef7f4016522c0fa
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 17:13:43 +0300

    types

--- frontend/src/app/types.ts
+++ frontend/src/app/types.ts
@@ -1,3 +1,5 @@
+import { token } from "./redux/user/userType";
+
 export interface SigninUser {
   email: string;
   password: string;
@@ -12,8 +14,21 @@ export interface IUser {
   name: string;
   email: string;
   token?: string;
-  avatar: string;
+  tokens: token[];
+  // tokens: token[] | undefined;
+  // avatar: string;
+  avatar: {
+    link: string;
+    name: string;
+  };
+  createdAt?: Date | string | number;
   [x: string]: unknown;
 }
 
-export type UpdUser = Pick<IUser, "email" | "name" | "password">;
+// export type UpdUser = Pick<IUser, "email" | "name" | "password">;
+export type UpdUser = {
+  email: string;
+  name: string;
+  password: string;
+  avatar: string | File;
+};
