e9d3bd5be125d651f5b23743f0705f6e284550d6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 17:16:44 +0300

    sidemenu

--- frontend/src/app/cryptoboard/Components/LogoutButton.tsx
+++ frontend/src/app/cryptoboard/Components/LogoutButton.tsx
@@ -1,31 +1,31 @@
 "use client";
-
 import { useRouter } from "next/navigation";
 import { useAppDispatch } from "../../redux/hooks";
 import { signout } from "../../redux/requests/authRequests";
-import { LogOut, LogOutIcon } from "lucide-react";
-
+import { LogOut } from "lucide-react";
+import toast from "react-hot-toast";
 const LogoutButton = () => {
   const dispatch = useAppDispatch();
   const navigate = useRouter();
-
   return (
     <button
       onClick={async () => {
         await dispatch(signout())
           .then(() => {
+            toast.success("Successfully logged out!");
             navigate.push("/auth/signin");
+            localStorage.setItem("page", "dashboard");
           })
           .catch((err: any) => {
             console.log(err);
+            toast.error("Successfully logged out!");
           });
       }}
-      className="cursor-pointer flex gap-3"
+      className="cursor-pointer flex pl-6 items-center gap-3 w-full h-full hover:text-[var(--error100)] rounded-[10px] hover:bg-[var(--error20)] "
     >
-      <LogOut className="size-5" />
-      Log out
+      {" "}
+      <LogOut className="size-5" /> Log out{" "}
     </button>
   );
 };
-
 export default LogoutButton;
--- frontend/src/app/cryptoboard/Components/sidemenu.tsx
+++ frontend/src/app/cryptoboard/Components/sidemenu.tsx
@@ -1,36 +1,163 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import LogoutButton from "./LogoutButton";
 import Link from "next/link";
-import { Home } from "lucide-react";
+import {
+  ChartLine,
+  ClipboardList,
+  FileChartColumnIncreasing,
+  Home,
+  Info,
+  Settings,
+  Wallet,
+  X,
+} from "lucide-react";
 
-const Sidemenu = () => {
-  const liStyle = "w-full h-10 flex justify-center items-center ";
-  const linkStyle =
-    "flex justify-center items-center gap-3 text-[var(--primary100)] w-full";
+const Sidemenu = ({
+  isOpen = false,
+  extraStyles,
+  isMob = false,
+  closeModal,
+}: {
+  isOpen?: boolean;
+  extraStyles?: string;
+  isMob?: boolean;
+  closeModal?: () => void;
+}) => {
+  const liStyle = "w-full h-10 flex justify-center items-center relative ";
+
+  const linkStyle = "flex pl-6 items-center gap-3 w-full";
+
+  type PageStates =
+    | "dashboard"
+    | "wallets"
+    | "analytics"
+    | "myportfolio"
+    | "settings"
+    | "help";
+
+  const localPage = localStorage.getItem("page") as PageStates | null;
+
+  const [chosenPage, chosePage] = useState<PageStates>(
+    localPage ?? "dashboard"
+  );
+
+  const setPage = (page: PageStates) => {
+    chosePage(page);
+    localStorage.setItem("page", page);
+  };
+
+  const clickHandle = () => {
+    if (isMob && closeModal) {
+      return closeModal();
+    } else {
+      //
+    }
+  };
+
+  const chosenStyle =
+    "text-[var(--primary100)] bg-[var(--primary20)] rounded-[10px] h-full sidemenuChosenItem";
 
   return (
-    <nav className="hidden min-[768px]:block bg-[var(--black0)] mts text-[var(--black60)] h-auto flex-0 py-6 px-[26px] min-[1440px]:px-[30px] ">
+    <nav
+      className={`${
+        isMob &&
+        `max-[767px]:fixed max-[767px]:top-0 max-[767px]:left-0 justify-center z-10 ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
+        } `
+      } w-full h-full flex flex-col  items-center min-[768px]:block bg-[var(--black0)] mts text-[var(--black60)] min-[768px]:h-auto flex-0 py-6 px-[26px] min-[1440px]:px-[30px] transition-transform duration-300 ${
+        extraStyles && extraStyles
+      }`}
+    >
+      <button
+        type="button"
+        className="absolute z-10 top-7 left-7 cursor-pointer block min-[768px]:hidden"
+        onClick={clickHandle}
+      >
+        <X />
+      </button>
       <ul className="w-40 flex flex-col gap-4 mb-8 min-[1440px]:w-48 ">
-        <li
-          // className={`${liStyle} rounded-[10px] bg-[var(--primary20)] relative `}
-          className={`${liStyle} rounded-[10px] bg-[var(--primary20)] relative `}
-        >
-          {/* <div className=" absolute h-full w-1 bg-[var(--primary100)] border rounded-l-[10px] left-0"></div> */}
-          <Link href={"/cryptoboard"} className={linkStyle}>
+        <li className={`${liStyle} relative `} onClick={clickHandle}>
+          <Link
+            href={"/cryptoboard"}
+            className={`${linkStyle} ${
+              chosenPage === "dashboard" ? chosenStyle : ""
+            } `}
+            onClick={() => setPage("dashboard")}
+          >
             <Home className="size-5" />
             Dashboard
           </Link>
         </li>
-        <li className={liStyle}>Analytics</li>
-        <li className={liStyle}>My Portfolio</li>
-        <li className={liStyle}>My wallets</li>
-        <li className={liStyle}>Exchanges</li>
+        <li className={`${liStyle} relative `} onClick={clickHandle}>
+          <Link
+            href={"/cryptoboard/analytics"}
+            className={`${linkStyle} ${
+              chosenPage === "analytics" ? chosenStyle : ""
+            } `}
+            onClick={() => setPage("analytics")}
+          >
+            <FileChartColumnIncreasing className="size-5" />
+            Analytics
+          </Link>
+        </li>
+        <li className={`${liStyle} relative `} onClick={clickHandle}>
+          <Link
+            href={"/cryptoboard/myportfolio"}
+            className={`${linkStyle} ${
+              chosenPage === "myportfolio" ? chosenStyle : ""
+            } `}
+            onClick={() => setPage("myportfolio")}
+          >
+            <ClipboardList className="size-5" />
+            My portfolio
+          </Link>
+        </li>
+        <li className={liStyle} onClick={clickHandle}>
+          <Link
+            href={"/cryptoboard/wallets"}
+            className={`${linkStyle} ${
+              chosenPage === "wallets" && chosenStyle
+            }`}
+            onClick={() => setPage("wallets")}
+          >
+            <Wallet className="size-5" />
+            My wallets
+          </Link>
+        </li>
+        <li className={liStyle} onClick={clickHandle}>
+          <span className={`${linkStyle} cursor-not-allowed opacity-50`}>
+            <ChartLine className="size-5" />
+            Exchanges
+          </span>
+        </li>
       </ul>
       <hr />
       <ul className="w-40 flex flex-col gap-4 mt-8 min-[1440px]:w-48 ">
-        <li className={liStyle}>Settings</li>
-        <li className={liStyle}>Help</li>
-        <li className={liStyle}>
+        <li className={liStyle} onClick={clickHandle}>
+          <Link
+            className={`${linkStyle} ${
+              chosenPage === "settings" && chosenStyle
+            }`}
+            href={"/settings"}
+            onClick={() => setPage("settings")}
+          >
+            <Settings className="szie-5" />
+            Settings
+          </Link>
+        </li>
+        <li className={liStyle} onClick={clickHandle}>
+          <Link
+            className={`${linkStyle} ${chosenPage === "help" && chosenStyle}`}
+            href={"/help"}
+            onClick={() => setPage("help")}
+          >
+            <Info className="size-5" />
+            Help
+          </Link>
+        </li>
+        <li className={liStyle} onClick={clickHandle}>
           <LogoutButton />
         </li>
       </ul>
