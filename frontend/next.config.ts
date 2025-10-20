3eef37167ba2389cd5eed8e8ba533bb6a63d7de0
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 15:59:09 +0300

    frontend update config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["coin-images.coingecko.com", "www.gravatar.com"],
+    domains: [
+      "coin-images.coingecko.com",
+      "www.gravatar.com",
+      "res.cloudinary.com",
+      "images.unsplash.com",
+      "plus.unsplash.com",
+    ],
   },
 };
 
