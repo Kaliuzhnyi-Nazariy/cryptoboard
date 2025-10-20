e62fcf7b4f2780628b88d12f3038096876dd2c3f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 17:23:04 +0300

    nav layout for showing the header and sidemenu

--- frontend/src/app/cryptoboard/NavLayout/NavLayout.tsx
+++ frontend/src/app/cryptoboard/NavLayout/NavLayout.tsx
@@ -6,12 +6,21 @@ const NavLayoutComponent = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-      <div className="flex flex-1 w-full h-auto">
-        <Sidemenu />
-        <div className="flex flex-2 w-full h-full items-center justify-center text-center flex-col min-[768px]:p-4 min-[1440px]:p-6 ">
+      {/* <div className="flex flex-1 w-full h-full">
+        <Sidemenu extraStyles="hidden min-[768px]:flex " />
+        {/* <div className="flex flex-2 w-full h-auto items-center justify-center text-center flex-col min-[768px]:p-4 min-[1440px]:p-6 ">
+          {children}
+        </div> */}
+      <div className="flex flex-1 w-full h-screen">
+        <Sidemenu extraStyles="hidden min-[768px]:flex " />
+        {/* <div className="flex flex-col flex-2 w-full items-center justify-start text-center min-[768px]:p-4 min-[1440px]:p-6 overflow-y-auto bg-blue-900">
+          {children}
+        </div> */}
+        <div className="flex flex-col flex-2 w-full h-auto items-center justify-start text-center min-[768px]:p-4 min-[1440px]:p-6 overflow-y-auto">
           {children}
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 };
