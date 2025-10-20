21de0376f202d25ef42da5fbcf7ae590a3b4a1a4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 17:15:22 +0300

    cryptoboard page

--- frontend/src/app/cryptoboard/page.tsx
+++ frontend/src/app/cryptoboard/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import TrendingMarket from "./dashboard/trendingMarket/TrendingMarket";
+import WalletStat from "./wallets/WalletStat";
+import Stats from "./dashboard/statistics/Stats";
+import Trade from "./dashboard/Trade/Trade";
+import QTransfers from "./dashboard/quickTransfers/QTransfers";
+import NewTokens from "./dashboard/newTokens/newTokens";
 
 const Page = () => {
   return (
-    <div className="w-full min-h-screen bg-[var(--black20)] text-[var(--black100)] flex items-center justify-center flex-col">
-      <p>Cryptoboard</p>
+    <div className="w-full  bg-[var(--black20)] text-[var(--black100)] flex items-center justify-center flex-col gap-2 min-[768px]:grid  min-[768px]:grid-cols-2 min-[768px]:grid-rows-auto min-[1440px]:grid-rows-[repeat(7,auto)] min-[1440px]:grid-cols-[2fr_1fr]  min-[1440px]:w-full min-[1440px]:gap-6 ">
+      <WalletStat />
+      <Stats />
       <TrendingMarket />
+      <Trade />
+      <QTransfers />
+      <NewTokens />
     </div>
   );
 };
