4b2d534f5e418a99b74b4801c41402f522d1d7fb
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 17:16:11 +0300

    custom input

--- frontend/src/app/auth/components/InputAuth.tsx
+++ frontend/src/app/auth/components/InputAuth.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 
 const InputAuth = ({
   type = "email",
   name,
   placeholder,
   required = false,
+  value,
 }: {
   type: string;
   name: string;
   placeholder?: string;
   required?: boolean;
+  value?: string;
 }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    if (value) {
+      if (name === "email") {
+        setInputValue(value);
+      } else if (name === "name") {
+        if (value.split("").includes("@")) {
+          const nameValue = value.split("@")[0];
+          setInputValue(nameValue);
+          localStorage.removeItem("address");
+        } else {
+          setInputValue(value);
+          localStorage.removeItem("address");
+        }
+      }
+    }
+  }, []);
+
   return (
     <input
       type={type}
@@ -18,6 +41,10 @@ const InputAuth = ({
       placeholder={placeholder}
       required={required ?? required}
       className="px-2 py-1 border-[1px] border-[var(--primary40)] focus-within:outline-[var(--primary100)] w-full min-[768px]:w-[500px] min-[768px]:mx-auto min-[768px]:px-3 min-[768px]:py-1.5 min-[1440px]:w-[700px] relative "
+      value={inputValue}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.currentTarget.value);
+      }}
     />
   );
 };
