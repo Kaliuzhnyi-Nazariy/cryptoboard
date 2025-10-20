70ca3adf519f7521423cf80ae68e4450b28523bd
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-20 15:57:28 +0300

    helpers

--- backend/helper/index.ts
+++ backend/helper/index.ts
@@ -3,3 +3,4 @@
 // module.exports = { mongooseHelper };
 export { default as mongooseHelper } from "./mongooseHelper";
 export { default as ctrlWrapper } from "./ctrlWrapper";
+export { default as postPhoto } from "./cloudinary";
