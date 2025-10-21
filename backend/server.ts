7e1896046d70c7c6ea0dc82ca8b93a16c3600816
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-21 10:44:01 +0300

    focus on deploying backend

--- backend/server.ts
+++ backend/server.ts
@@ -8,13 +8,13 @@ import app from "./app";
 
 const { PORT, DB_HOST } = process.env;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.all("/{*any}", (_req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  });
-}
+//   app.all("/{*any}", (_req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 mongoose
   .connect(DB_HOST as string)
