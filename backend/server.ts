df320a7996875d403997e52b3c7dd6e0725ba750
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-24 17:21:52 +0300

    add frontend

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
+import express from "express";
+import path from "path";
 dotenv.config();
 
 import mongoose from "mongoose";
@@ -6,13 +8,13 @@ import app from "./app";
 
 const { PORT, DB_HOST } = process.env;
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.all("/{*any}", (_req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-//   });
-// }
+  app.all("/{*any}", (_req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 mongoose
   .connect(DB_HOST as string)
