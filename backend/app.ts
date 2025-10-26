499e150e754711928bd541f5c29ef784ba607f36
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-26 17:17:54 +0200

    upd cors

--- backend/app.ts
+++ backend/app.ts
@@ -11,6 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://cryptoboard-unor.onrender.com"],
+    methods: "PUT, GET, POST",
     credentials: true,
   })
 );
