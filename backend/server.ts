9e8100daca36075f304a2c708cc35e760bf28f7d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-10-29 12:02:20 +0200

    changed deploying file

--- backend/server.ts
+++ backend/server.ts
@@ -9,14 +9,14 @@ import app from "./app";
 const { PORT, DB_HOST } = process.env;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.all("/{*any}", (req, res) => {
     if (req.path.startsWith("/api")) {
       // Let API routes handle it
       return res.status(404).json({ message: "API route not found" });
     }
-    res.sendFile(path.resolve(__dirname, "../frontend/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist"));
   });
 }
 
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  distDir: "../dist/frontend",
+  // output: "export",
+  distDir: "dist",
   images: {
     unoptimized: true,
     domains: [
