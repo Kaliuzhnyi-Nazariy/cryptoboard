360c4a188e397f45129a4f9ae18bb956ec4b545a
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-04-07 23:27:18 +0200

    feat: change fetch user

--- src/app/Layouts/GetUserLayout.tsx
+++ src/app/Layouts/GetUserLayout.tsx
@@ -3,26 +3,32 @@
 import { useSelector } from "react-redux";
 import { isLoading } from "../redux/selectors";
 import useAuthMiddleware from "./helper/authMiddleware";
+import { useEffect } from "react";
+import { useAppDispatch } from "../redux/hooks";
+import { getMe } from "../redux/requests/userRequests";
 
 const GetUserLayout = ({ children }: { children: React.ReactNode }) => {
   const loading = useSelector(isLoading);
 
-  const { loadingUser } = useAuthMiddleware();
-
-  if (loadingUser) {
-    return <h1>loading...</h1>;
-  } else
-    return (
-      <>
-        {loading ? (
-          <div className="w-full  flex items-center justify-center">
-            Loadnig...
-          </div>
-        ) : (
-          <>{children}</>
-        )}
-      </>
-    );
+  const dispatch = useAppDispatch();
+  // const { loadingUser } = useAuthMiddleware();
+  useEffect(() => {
+    dispatch(getMe());
+  }, []);
+  // if (loadingUser) {
+  //   return <h1>loading...</h1>;
+  // } else
+  return (
+    <>
+      {loading ? (
+        <div className="w-full  flex items-center justify-center">
+          Loadnig...
+        </div>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
 };
 
 export default GetUserLayout;
